6ddc1c7a3fea1d59bd9e8a06d497b542c0b8f370
JingyuanHe1222 <jingyuanhe1222@gmail.com>
2025-03-28 10:30:28 -0400

    utility update: trec_eval truth read and write to diskANN format

--- diskann/utils.py
+++ diskann/utils.py
@@ -3,6 +3,26 @@ import os
 import pickle
 
 
+
+
+def read_trec_qrels(path): 
+    qid_qrels = {}
+    with open(path, "r") as f:
+        for line in f:
+            parts = line.strip().split("\t")
+            qid = parts[0]
+            qrel = parts[2]
+            qid_qrels[qid] = qrel
+    return qid_qrels
+
+
+def get_qrels_array(qid_qrels_dict): 
+    qrels = []
+    for qid in qid_qrels_dict: 
+        qrels.append(qid_qrels_dict[qid])
+    return np.array(qrels)[:, None]
+
+
 def read_fbin(filename, start_idx=0, chunk_size=None):
     with open(filename, "rb") as f:
         nvecs, dim = np.fromfile(f, count=2, dtype=np.int32)
@@ -15,24 +35,68 @@ def read_fbin(filename, start_idx=0, chunk_size=None):
     return arr.reshape(nvecs, dim)
 
 
-def retrieval_result_read(fname):
+def retrieval_result_read(fname, e2e=False):
+    """
+    Read the binary ground truth file in DiskANN format. 
+    If e2e is given as True, no distances array will be read (end of end qrel scenario). 
+    """
     n, d = map(int, np.fromfile(fname, dtype="uint32", count=2))
     print("n: ", n)
     print("d: ", d)
-    assert os.stat(fname).st_size == 8 + n * d * (4 + 4)
+    # validity check 
+    if e2e: 
+        assert os.stat(fname).st_size == 8 + n * d * 4
+    else: 
+        assert os.stat(fname).st_size == 8 + n * d * (4 + 4)
+    
     f = open(fname, "rb")
     f.seek(4+4)
+
+    I, D = None, None
     I = np.fromfile(f, dtype="int32", count=n * d).reshape(n, d)
-    D = np.fromfile(f, dtype="float32", count=n * d).reshape(n, d)
+    if not e2e: 
+        D = np.fromfile(f, dtype="float32", count=n * d).reshape(n, d)
+
     return I, D
 
 
 def write_embed_to_binary(embeddings, output_path): 
+    """
+    Write the embedding array into a binary file in ANN-Indexing (DiskANN, SPTAG) format. 
+    The content of the output file can be access through: embeds = read_fbin(output_path)
+    """
     num, dim = embeddings.shape
     with open(output_path, "wb") as f:
         f.write(num.to_bytes(4, 'little'))
         f.write(dim.to_bytes(4, 'little'))
         f.write(embeddings.tobytes())
+
+
+def write_qrels_to_binary(qid_qrels_dict, output_binary_path, docid_one_index=False): 
+    """
+    Write the qrels given in a dictionary to ANN-Indexing (DiskANN, SPTAG) binary format. 
+    Currently only 1 qrel per query allowed. 
+    The content of the output file can be access through: I, _ = retrieval_result_read(output_binary_path, e2e=True)
+    """
+    nrows = len(qid_qrels_dict)
+    ncols = 1
+
+    qrels = get_qrels_array(qid_qrels_dict)
+    # convert to 0-indexing if needed as DiskANN arranged by candidate index in corpus 
+    if docid_one_index: 
+        qrels -= 1
+
+    with open(output_binary_path, "wb") as f:
+        offset = 0
+        # header
+        f.write(nrows.to_bytes(4, 'little')) # number of points
+        f.write(ncols.to_bytes(4, 'little')) # dimension
+        offset += 8
+        
+        # ids
+        f.seek(offset)
+        f.write(qrels.astype('uint32').tobytes()) 
+        offset += qrels.nbytes
 
 
 def write_docids_to_pkl(docids, output_path): 
